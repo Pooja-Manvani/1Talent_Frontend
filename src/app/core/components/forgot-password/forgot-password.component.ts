0357c493105bccaa730f59a6979f2afe36e5c3bd
ChiragPatel3001 <cp221740@gmail.com>
2022-03-22 17:54:13 +0530

    feat: forgotpassword_htmlComplete

--- src/app/core/components/forgot-password/forgot-password.component.ts
+++ src/app/core/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
-
-  constructor() { }
+  forgotpassword: FormGroup;
+  constructor( private fb: FormBuilder) { 
+    this.forgotpassword = this.fb.group({
+      email: ['', [Validators.required]],
+    })
+  }
 
   ngOnInit(): void {
   }
-
+  onSubmit(){
+    
+  }
 }
