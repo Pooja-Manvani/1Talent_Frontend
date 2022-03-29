fb1fddd62536bb8b88b66d3beb1535953b28e6a4
Ankitkumar Patel <ankitkumar.patel@1Rintern.local>
2022-03-29 13:17:21 +0530

    Feat: Profile UI Design

---
+++ src/app/profile/profile-container/profile-presentation/personal-details-prensentation/personal-details-prensentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-details-prensentation',
+  templateUrl: './personal-details-prensentation.component.html',
+  styleUrls: ['./personal-details-prensentation.component.scss']
+})
+export class PersonalDetailsPrensentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
