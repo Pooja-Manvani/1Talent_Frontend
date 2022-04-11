8fa06875a73fa21c314b5ed1beec7af1252597cd
Jigaboy10 <jigard.bhanushali12@gmail.com>
2022-04-11 19:09:48 +0530

    feat: implemented signout service

--- src/app/core/components/sidebar/sidebar.component.ts
+++ src/app/core/components/sidebar/sidebar.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
 })
 export class SidebarComponent {
+
+  constructor(private _service: AuthService,private _router : Router){
+
+  }
+  /**
+   * @name signOut
+   * @description service call for token removal and route to login page
+   */
+  public signOut(){
+    this._service.clearLocalStorage()
+    this._router.navigateByUrl("/login")
+  }
 }
