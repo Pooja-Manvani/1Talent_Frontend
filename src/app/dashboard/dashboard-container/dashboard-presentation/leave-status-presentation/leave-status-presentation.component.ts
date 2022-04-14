ab5da390e465960d87c9643f46452a542f512904
shreyapatel-11 <shr1390pate@gmail.com>
2022-04-14 12:04:14 +0530

    feat: added google-chart library & implemented donut chart

--- src/app/dashboard/dashboard-container/dashboard-presentation/leave-status-presentation/leave-status-presentation.component.ts
+++ src/app/dashboard/dashboard-container/dashboard-presentation/leave-status-presentation/leave-status-presentation.component.ts
@@ -1,8 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-leave-status-presentation',
   templateUrl: './leave-status-presentation.component.html',
 })
 export class LeaveStatusPresentationComponent {
+
+  @ViewChild('pieChart')
+  pieChart!: ElementRef;
+
+  drawChart = () => {
+    const data = google.visualization.arrayToDataTable([
+      ['Task', 'Hours per Day'],
+      ['Accepted', 8],
+      ['Rejected', 2],
+      ['Pending', 2],
+      ['Req. Revoke', 0],
+      ['Revoked', 0]
+    ]);
+  
+    const options = {
+      pieHole: 0.8,
+      // legend: {position: 'top'},
+    };
+  
+    const chart = new google.visualization.PieChart(this.pieChart.nativeElement);
+  
+    chart.draw(data, options);
+  }
+  
+    ngOnInit() {
+      google.charts.load('current', { 'packages': ['corechart'] });
+      google.charts.setOnLoadCallback(this.drawChart);
+    }
 }
